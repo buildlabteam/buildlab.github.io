1c31bd32f8df8c4d25d009d8c43a359ade52faec
Leonardo Alves(leo-pro) <leoroberto.alves@hotmail.com>
2020-06-14 18:30:27 -0300

    Initial templates of new landing page

---
+++ curso-logica-de-programacao/index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Build Lab Academy - Curso Lógica de programação</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- favicon icons -->
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="apple-touch-icon" sizes="57x57" href="../assets/img/icons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="../assets/img/icons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="../assets/img/icons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/icons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="../assets/img/icons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="../assets/img/icons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="../assets/img/icons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="../assets/img/icons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="../assets/img/icons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="../assets/img/icons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="../assets/img/icons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/img/icons/favicon-16x16.png">
+    <link rel="manifest" href="../assets/img/icons/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="../assets/img/icons/ms-icon-144x144.png">
+    <meta name="theme-color" content="#222325">
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="stylesheet" href="css/main.css">
+
+</head>
+
+<body class="background-black-buildlab" data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+    <div class="site-wrap" id="home-section">
+      <header class="site-navbar pt-5 site-navbar-target" role="banner">
+          <div class="container-fluid">
+              <!-- <div class="row d-flex justify-content-center">
+                <a href="index.html" class="h2 mb-0"><img src="images/logo-academy.png" width="250" alt=""></a>
+              </div> -->
+              <div class="row d-flex justify-content-center">
+              <div id="typed-strings">
+                <p><span>QUER APRENDER A PROGRAMAR?</span></p>
+              </div>
+                <h1 class="title-landing-page"><span id="typed"></span></h1>
+              </div>
+          </div>
+      </header>
+
+      <section class="container">
+        <div class="row video-descri">
+          <div class="col-md-6">
+            <h1 class="title-principal">Lógica de Programação</h1>
+            <p class="mb-3">Nesse curso 100% Online e 100% Gratuito você vai dar seus primeiros passos no mundo da programação.</p>
+            <div class="mb-4" id="curso-info">
+              <h5 class="my-3"><i class="fas fa-video"></i> +60 Aulas</h5>
+              <h5 class="my-3"><i class="fas fa-pen-alt"></i> +70 Exercícios</h5>
+              <h5 class="mt-3 mb-5"><i class="fas fa-file-alt"></i> Material didático</h5>
+            </div>
+            <a class="btn-submit" href="#modalInscricao">Fazer a inscrição</a>
+          </div>
+          <div class="col-md-6">
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/3XssffgnsU4"></iframe>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="site-section" id="blog-section">
+        <div class="container">
+          <div class="row mb-5">
+              <div class="col-12 text-center">
+                  <h2 class="section-title mb-3">Depoimento dos alunos</h2>
+              </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+                <div class="h-entry">
+                  <iframe width="360" height="220" src="https://www.youtube.com/embed/m36BVBAZzI8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  <h4>Leonardo Alves</h4>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+                <div class="h-entry">
+                  <iframe width="360" height="220" src="https://www.youtube.com/embed/m36BVBAZzI8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  <h4>Leonardo Alves</h4>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+                <div class="h-entry">
+                <iframe width="360" height="220" src="https://www.youtube.com/embed/m36BVBAZzI8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  <h4>Leonardo Alves</h4>
+                </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div class="footer py-5 text-center">
+        <div class="container">
+          <!-- <div class="row mb-5">
+              <div class="col-12">
+                  <p class="mb-0">
+                      <a href="#" class="p-3"><span class="icon-facebook"></span></a>
+                      <a href="#" class="p-3"><span class="icon-twitter"></span></a>
+                      <a href="#" class="p-3"><span class="icon-instagram"></span></a>
+                      <a href="#" class="p-3"><span class="icon-linkedin"></span></a>
+                      <a href="#" class="p-3"><span class="icon-pinterest"></span></a>
+                  </p>
+              </div>
+          </div> -->
+          <div class="row">
+              <div class="col-md-12 d-flex justify-content-between align-items-center">
+                <img src="images/logo-academy.png" width="200" alt="">
+                <p class="mb-0">
+                    Copyright &copy;
+                    <script>
+                        document.write(new Date().getFullYear());
+                    </script>
+                    All rights reserved <a href="https://buildlab.com.br/" target="_blank">Build Lab</a>
+                </p>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Fontawesome icons -->
+    <script src="https://kit.fontawesome.com/a41e0085e0.js" crossorigin="anonymous"></script>
+    
+    <!-- Jquery-->
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+
+    <!-- Bootstrap JS-->
+    <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/jquery-ui.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/bootstrap-datepicker.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+
+    <!-- TypeWriter JS -->
+  	<script type="text/javascript" src="../assets/js/lib/typed.min.js"></script>
+
+    <script type="text/javascript">
+      var typed = new Typed('#typed', {
+        stringsElement: '#typed-strings',
+        typeSpeed: 150,
+        loop: false,
+        cursorChar: '_'
+      });
+    </script>
+
+    <script src="js/main.js"></script>
+</body>
+</html>
---
+++ curso-logica-de-programacao/inscricao-confirmada/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Inscricação confirmado no Curso de Lógica de Programação!</title>
+
+    <!-- favicon icons -->
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="apple-touch-icon" sizes="57x57" href="../../assets/img/icons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="../../assets/img/icons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="../../assets/img/icons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="../../assets/img/icons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="../../assets/img/icons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="../../assets/img/icons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="../../assets/img/icons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="../../assets/img/icons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="../../assets/img/icons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="../../assets/img/icons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../../assets/img/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="../../assets/img/icons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../../assets/img/icons/favicon-16x16.png">
+    <link rel="manifest" href="../../assets/img/icons/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="../../assets/img/icons/ms-icon-144x144.png">
+    <meta name="theme-color" content="#222325">
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/jquery-ui.css">
+    <link rel="stylesheet" href="../css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="../css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="../css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="../css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="../css/aos.css">
+
+    <link rel="stylesheet" href="../css/style.css">
+
+    <link rel="stylesheet" href="../css/main.css">
+
+    <style>
+      body{
+        background: #0f0f0f;
+        padding-top: 100px;
+      }
+      h4{
+        color: #fff;
+        font-weight: bold;
+        font-size: 1.9em;
+      }
+      p{
+        color: #fff;
+        font-size: 0.9em;
+      }
+      a{
+        color: #fff;
+      }
+    </style>
+</head>
+<body>
+    <div class="container d-flex flex-column align-items-center">
+      <div class="row mb-5">
+        <img src="../images/logo-academy.png" width="300" alt="">
+      </div>
+      <div class="col-7 text-center">
+        <h4 class="mb-5">Sua inscricação está confirmada!</h4>
+        <p>
+          Fique atento ao seu email, pois enviaremos o link. Se caso notar que está demorando, verifique sua caixa de <i>spam</i> ou <i>lixo eletrônico</i>. Não deixe de nos seguir no <a href="https://instagram.com/build_lab">Instagram</a> para acompanhar as novidades.
+        </p>
+      </div>
+    </div>
+</body>
+</html>
