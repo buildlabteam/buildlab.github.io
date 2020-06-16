2aed8b4e3715d7095ef322e621ac58b99696cef4
Leonardo Alves(leo-pro) <leoroberto.alves@hotmail.com>
2020-06-16 16:46:15 -0300

    Form into modal

--- curso-logica-de-programacao/index.php
+++ curso-logica-de-programacao/index.php
@@ -28,6 +28,7 @@
 
     <link rel="stylesheet" href="fonts/icomoon/style.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/form.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/main.css">
 
@@ -36,14 +37,14 @@
 <body class="background-black-buildlab">
     
       <header class="pt-5">
-          <div class="container-fluid">
-              <div class="row d-flex justify-content-center">
-                <div id="typed-strings">
-                  <p><span>QUER APRENDER A PROGRAMAR?</span></p>
-                </div>
-                <h1 class="title-landing-page"><span id="typed"></span></h1>
-              </div>
+        <div class="container-fluid">
+          <div class="row d-flex justify-content-center">
+            <div id="typed-strings">
+              <p><span>QUER APRENDER A PROGRAMAR?</span></p>
+            </div>
+            <h1 class="title-landing-page"><span id="typed"></span></h1>
           </div>
+        </div>
       </header>
 
       <section class="container">
@@ -65,7 +66,9 @@
                 <p>Material didático</p>
               </div>
             </div>
-            <div class="text-center"><a class="btn-submit" href="#modalInscricao">Fazer a inscrição</a></div>
+            <div class="text-center">
+              <button type="button" class="btn btn-submit" data-toggle="modal" data-target="#modalInscricao">Inscrever-se agora!</button>
+            </div>
           </div>
           <div class="col-10 col-md-6 order-1 order-md-2">
             <div class="embed-responsive embed-responsive-16by9">
@@ -108,53 +111,117 @@
 
       <div class="footer py-5 text-center">
         <div class="container">
-          <!-- <div class="row mb-5">
-              <div class="col-12">
-                  <p class="mb-0">
-                      <a href="#" class="p-3"><span class="icon-facebook"></span></a>
-                      <a href="#" class="p-3"><span class="icon-twitter"></span></a>
-                      <a href="#" class="p-3"><span class="icon-instagram"></span></a>
-                      <a href="#" class="p-3"><span class="icon-linkedin"></span></a>
-                      <a href="#" class="p-3"><span class="icon-pinterest"></span></a>
-                  </p>
-              </div>
-          </div> -->
           <div class="row">
-              <div class="col-md-12 d-flex flex-column flex-md-row justify-content-between align-items-center">
-                <img src="images/logo-academy.png" id="logo-academy">
-                <p class="mb-0" id="copyright">
-                    Copyright &copy;
-                    <script>
-                        document.write(new Date().getFullYear());
-                    </script>
-                    All rights reserved <a href="https://buildlab.com.br/" target="_blank">Build Lab</a>
-                </p>
-              </div>
+            <div class="col-md-12 d-flex flex-column flex-md-row justify-content-between align-items-center">
+              <img src="images/logo-academy.png" id="logo-academy">
+              <p class="mb-0" id="copyright">
+                Copyright &copy;
+                <script>
+                    document.write(new Date().getFullYear());
+                </script>
+                All rights reserved <a href="https://buildlab.com.br/" target="_blank">Build Lab</a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
     
 
+     <!-- Modal -->
+     <div class="modal fade" id="modalInscricao" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <div class="w-100 ml-2 d-flex justify-content-center">
+                <img src="images/logo-academy.png" width="150px">
+              </div>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true" style="color: #fff"><i class="fas fa-times"></i></span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form method="POST" class="form-signin">
+                <!-- <h1 class="h5 mb-5">INSCREVA-SE AGORA!</h1> -->
+                <label for="inputName" class="sr-only">Nome</label>
+                <input type="text" id="inputName" name="name" class="form-control" placeholder="Nome" required>
+                <label for="inputEmail" class="sr-only">E-mail</label>
+                <input type="email" name="inputEmail" name="email" id="inputEmail" class="form-control mb-3" placeholder="Seu melhor E-mail" required>
+                <label for="inputName" class="sr-only">Telefone</label>
+                <input type="text" id="inputTelefone" name="phone" class="form-control" placeholder="Telefone">
+                <label for="inputName" class="sr-only">Instituição</label>
+                <input type="text" id="inputTelefone" name="company" class="form-control mb-3" placeholder="Instituição de ensino ou empresa" required>
+                <button class="btn btn-submit btn-block mb-4" name="submit" type="submit"><b>Quero aprender!</b></button>
+                <p class="extra-info text-center"><span class="fas fa-lock"></span> Os dados informados estão seguros</p>
+              </form>
+            </div>
+            <!-- <div class="modal-footer">
+              <div class="row mb-5">
+                <div class="col-12">
+                  <p class="mb-0">
+                    <a href="#" class="p-3"><span class="icon-facebook"></span></a>
+                    <a href="#" class="p-3"><span class="icon-twitter"></span></a>
+                    <a href="#" class="p-3"><span class="icon-instagram"></span></a>
+                    <a href="#" class="p-3"><span class="icon-linkedin"></span></a>
+                    <a href="#" class="p-3"><span class="icon-pinterest"></span></a>
+                  </p>
+                </div>
+              </div>
+            </div> -->
+          </div>
+        </div>
+      </div>
+      <?php
+        if ($_POST) {
+          $name = filter_input(INPUT_POST, 'name');
+          $email = strtolower(filter_input(INPUT_POST, 'email'));
+          $phone = filter_input(INPUT_POST, 'phone');
+          $company = filter_input(INPUT_POST, 'company');
+          $array = ['name' => $name, 'email' => $email, 'phone' => $phone, 'company' => $company];
+          $json_filename = 'data_users.json';
+          $old_json = file_get_contents($json_filename);
+          $old_data = json_decode($old_json);
+
+          if ($old_data == null) {
+            $old_data = array();
+          }
+
+          $new_data = array_push($old_data, $array);
+          $new_json = json_encode($old_data);
+          file_put_contents($json_filename, $new_json);
+
+          $url = 'https://mvcdfme4gf.execute-api.us-east-1.amazonaws.com/prod/api/AddEmail';
+          $data = array("email" => $email, "name" => $name, "company" => $company, "phone" => $phone, "auth" => "BUILD@)!(*!#()JIAOKDLAM<D");
+
+          $postdata = json_encode($data);
+
+          $ch = curl_init($url);
+          curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+          curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+          curl_setopt($ch, CURLOPT_POST, 1);
+          curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+          curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+          curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+          $result = curl_exec($ch);
+          curl_close($ch);
+
+          echo "<script>window.location.replace('https://buildlab.com.br/aprenda-a-programar/sucesso');</script>";
+        }
+      ?>
+
+
     <!-- Fontawesome icons -->
     <script src="https://kit.fontawesome.com/a41e0085e0.js" crossorigin="anonymous"></script>
-    
     <!-- Jquery-->
     <script type="text/javascript" src="js/jquery.min.js"></script>
-
     <!-- Bootstrap JS-->
-    <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/jquery-ui.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <script src="js/popper.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/jquery.countdown.min.js"></script>
-    <script src="js/bootstrap-datepicker.min.js"></script>
     <script src="js/jquery.easing.1.3.js"></script>
-    <script src="js/aos.js"></script>
-    <script src="js/jquery.fancybox.min.js"></script>
     <script src="js/jquery.sticky.js"></script>
 
     <!-- TypeWriter JS -->
-  	<script type="text/javascript" src="../assets/js/lib/typed.min.js"></script>
+  	<script type="text/javascript" src="js/typed.js"></script>
 
     <script type="text/javascript">
       var typed = new Typed('#typed', {
@@ -165,6 +232,6 @@
       });
     </script>
 
-    <script src="js/main.js"></script>
+    <!-- <script src="js/modal.js"></script> -->
 </body>
 </html>
